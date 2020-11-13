2643cf4f45a9c7e62e581170e0718f15ab2bbe5b
Arjun Dureja <arjun.dureja1000@gmail.com>
2020-11-13 18:25:09 -0500

    update styles

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const iconURL = "https://api.appfigures.com/v2/reports/sales/?group_by=products&client_key=1be40558c1de4197b1629674dab0fe62";
+export const ratingURL = "https://api.appfigures.com/v2/reports/ratings/?group_by=product&client_key=1be40558c1de4197b1629674dab0fe62"
+export const authKey = "YXJqdW4uZHVyZWphMTAwMEBnbWFpbC5jb206YXJqdW44NzM="
