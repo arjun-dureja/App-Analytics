3618fe3a32a45ce2005589890040ce0614c1744f
Spearman873 <arjun.dureja4000@gmail.com>
2021-01-25 18:49:52 -0500

    remove ratings (paid api)

--- src/Analytics.js
+++ src/Analytics.js
@@ -1,120 +1,109 @@
-import React from 'react'
-import Loader from 'react-loader-spinner'
-import { iconURL, ratingURL, linkURL, authKey } from './constants'
+import React from "react";
+import Loader from "react-loader-spinner";
+import { iconURL, linkURL, authKey } from "./constants";
 
 export default class Analytics extends React.Component {
+  state = {
+    loading: true,
+    icons: [],
+    downloads: [],
+    links: [],
+  };
 
-    state = {
-        loading: true,
-        icons: [],
-        downloads: [],
-        ratings: [],
-        links: []
+  async componentDidMount() {
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const iconDownloadsURL = proxy + iconURL;
+    const response = await fetch(iconDownloadsURL, {
+      method: "post",
+      headers: new Headers({
+        Authorization: "Basic " + authKey,
+      }),
+    });
+    const data = await response.json();
+    for (let key in data) {
+      if (data[key]["product"]["icon"] !== "Unknown") {
+        this.setState((prevState) => ({
+          icons: [...prevState.icons, data[key]["product"]["icon"]],
+          downloads: [...prevState.downloads, data[key]["downloads"]],
+        }));
+      }
     }
 
-    async componentDidMount() {
-        const proxy = "https://cors-anywhere.herokuapp.com/";
-        const iconDownloadsURL = proxy + iconURL;
-        const response = await fetch(iconDownloadsURL, {
-            method: 'post',
-            headers: new Headers({
-                'Authorization': 'Basic ' + authKey
-            })
-        });
-        const data = await response.json();
-        for (let key in data) {
-            if (data[key]['product']['icon'] !== "Unknown") {
-                this.setState(prevState => ({
-                    icons: [...prevState.icons, data[key]['product']['icon']],
-                    downloads: [...prevState.downloads, data[key]['downloads']]
-                }))
-            }
-        }
-
-        const ratingsURL = proxy + ratingURL;
-        const ratingsResponse = await fetch(ratingsURL, {
-            method: 'post',
-            headers: new Headers({
-                'Authorization': 'Basic ' + authKey
-            })
-        });
-        const ratingsData = await ratingsResponse.json();
-        for (let app in ratingsData) {
-            this.setState(prev => ({
-                ratings: [...prev.ratings, ratingsData[app]['average']],
-            }))
-        }
-
-        const linksURL = proxy + linkURL;
-        const linksResponse = await fetch(linksURL, {
-            method: 'get',
-            headers: new Headers({
-                'Authorization': 'Basic ' + authKey
-            })
-        });
-        const linksData = await linksResponse.json()
-        for (let link in linksData) {
-            if (linksData[link]['icon'] !== 'Unknown') {
-                this.setState(prevLink => ({
-                        links: [...prevLink.links, linksData[link]['view_url']]
-                }))
-            }
-        }
-
-        this.setState({
-            icons: this.state.icons.reverse(),
-            downloads: this.state.downloads.reverse(),
-            ratings: this.state.ratings.reverse(),
-            links: this.state.links.reverse(),
-            loading: false
-        });
+    const linksURL = proxy + linkURL;
+    const linksResponse = await fetch(linksURL, {
+      method: "get",
+      headers: new Headers({
+        Authorization: "Basic " + authKey,
+      }),
+    });
+    const linksData = await linksResponse.json();
+    for (let link in linksData) {
+      if (linksData[link]["icon"] !== "Unknown") {
+        this.setState((prevLink) => ({
+          links: [...prevLink.links, linksData[link]["view_url"]],
+        }));
+      }
     }
 
-    render() {
-        return (
-            <div className="container">
-                    {
-                        this.state.loading ? 
-                            <Loader
-                                className="loader"
-                                type="Oval"
-                                color="#B969F5"
-                                height={80}
-                                width={80}
-                                timeout={5000} //5 secs
-                            />
-                        : 
-                        <div className="box">
-                            <div className="title">
-                                <h1>App Analytics</h1>
-                            </div>
-                            <div className="column-titles">
-                                <h2>App</h2>
-                                <h2>Downloads</h2>
-                                <h2>Rating</h2>
-                            </div>
-                            <div className="analytics">
-                                <div className="app-icons">
-                                    {this.state.links.map((link, i) =>
-                                        <a className="link" key={i} href={link} target="_blank" rel="noreferrer">
-                                            <img className="img" key={i} src={this.state.icons[i]} alt="Icon" />
-                                        </a>
-                                    )}
-                                </div>
-                                <div className="downloads">
-                                    {this.state.downloads.map((count, i) =>
-                                        <h3 className="h3" key={i}>{count}</h3>,
-                                    )}
-                                </div>
-                                <div className="ratings">
-                                    {this.state.ratings.map((rating, i) =>
-                                        <h3 className="h3" key={i}>{rating} ⭐</h3>
-                                    )}
-                                </div>
-                            </div>
-                        </div>
-                    }
+    this.setState({
+      icons: this.state.icons.reverse(),
+      downloads: this.state.downloads.reverse(),
+      links: this.state.links.reverse(),
+      loading: false,
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        {this.state.loading ? (
+          <Loader
+            className="loader"
+            type="Oval"
+            color="#be72fa"
+            height={80}
+            width={80}
+            timeout={5000} //5 secs
+          />
+        ) : (
+          <div className="box">
+            <div className="title">
+              <h1>App Analytics</h1>
             </div>
-        );
-    }
+            <div className="column-titles">
+              <h2>App</h2>
+              <h2>Downloads</h2>
+            </div>
+            <div className="analytics">
+              <div className="app-icons">
+                {this.state.links.map((link, i) => (
+                  <a
+                    className="link"
+                    key={i}
+                    href={link}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      className="img"
+                      key={i}
+                      src={this.state.icons[i]}
+                      alt="Icon"
+                    />
+                  </a>
+                ))}
+              </div>
+              <div className="downloads">
+                {this.state.downloads.map((count, i) => (
+                  <h3 className="h3" key={i}>
+                    {count}
+                  </h3>
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
-export const iconURL = "https://api.appfigures.com/v2/reports/sales/?group_by=products&client_key=1be40558c1de4197b1629674dab0fe62";
-export const ratingURL = "https://api.appfigures.com/v2/reports/ratings/?group_by=product&client_key=1be40558c1de4197b1629674dab0fe62"
-export const linkURL = "https://api.appfigures.com/v2/products/mine/?meta=true&client_key=1be40558c1de4197b1629674dab0fe62"
-export const authKey = "YXJqdW4uZHVyZWphMTAwMEBnbWFpbC5jb206ZmFrZXBhc3N3b3Jk"
+export const iconURL =
+  "https://api.appfigures.com/v2/reports/sales/?group_by=products&client_key=1be40558c1de4197b1629674dab0fe62";
+export const linkURL =
+  "https://api.appfigures.com/v2/products/mine/?meta=true&client_key=1be40558c1de4197b1629674dab0fe62";
+export const authKey = "YXJqdW4uZHVyZWphMTAwMEBnbWFpbC5jb206ZmFrZXBhc3N3b3Jk";
