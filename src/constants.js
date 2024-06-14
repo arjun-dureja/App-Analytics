6e137d9f78b0f9e250d214e3789aa8eda7c1da40
Arjun Dureja <arjun.dureja1000@gmail.com>
2024-06-14 19:06:08 -0400

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,4 @@ export const iconURL =
   'https://api.appfigures.com/v2/reports/sales/?group_by=products&client_key=1be40558c1de4197b1629674dab0fe62';
 export const linkURL =
   'https://api.appfigures.com/v2/products/mine/?meta=true&client_key=1be40558c1de4197b1629674dab0fe62';
-export const authKey = 'YXJqdW4uZHVyZWphMTAwMEBnbWFpbC5jb206ZmFrZXBhc3N3b3Jk';
+export const authKey = 'pat_VTTL34Ues7cT6h2437DO4lE0kxmIhQbo';
