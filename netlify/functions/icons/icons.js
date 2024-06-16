390efb65d23d4c4235436390eb0397aa1e919190
arjun-dureja <arjun.dureja1000@gmail.com>
2024-06-16 17:57:27 -0400

    Add back event

--- netlify/functions/icons/icons.js
+++ netlify/functions/icons/icons.js
@@ -3,7 +3,7 @@ const { fetchData } = require("../utils/api");
 const iconURL =
   'https://api.appfigures.com/v2/reports/sales/?group_by=products&client_key=1be40558c1de4197b1629674dab0fe62';
 
-const handler = async () => {
+const handler = async (event) => {
   try {
     const data = await fetchData(iconURL)
     return {
--- netlify/functions/links/links.js
+++ netlify/functions/links/links.js
@@ -3,7 +3,7 @@ const { fetchData } = require("../utils/api");
 const linkURL =
   'https://api.appfigures.com/v2/products/mine/?meta=true&client_key=1be40558c1de4197b1629674dab0fe62';
 
-const handler = async () => {
+const handler = async (event) => {
   try {
     const data = await fetchData(linkURL)
     return {
